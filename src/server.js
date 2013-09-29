c1be32d9713563f22c37532fb0e990cd17fc8c49
Quinn C. Hoyer <quinn@themightyquinn.com>
2013-09-29 13:31:54 -0700

    whatever

--- src/server.js
+++ src/server.js
@@ -1,16 +1,4 @@
 /**
  * Created by quinn on 9/29/13.
  */
-
-var restify = require('restify');
-
-var server = restify.createServer();
-
-server.get('/', restify.serveStatic({
-    directory: './static/html',
-    default: 'index.html'
-}));
-
-server.listen(process.env.PORT || 8080, function () {
-    console.log('%s listening at %s', server.name, server.url);
-});
+console.log(JSON.stringify(process.env));
