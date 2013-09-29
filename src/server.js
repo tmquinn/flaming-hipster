24e2bf97e1043561a77cecf48d3fb688ee007a38
Quinn C. Hoyer <quinn@themightyquinn.com>
2013-09-29 14:09:40 -0700

    dskfj

--- src/server.js
+++ src/server.js
@@ -1,4 +1,33 @@
 /**
  * Created by quinn on 9/29/13.
  */
-console.log(JSON.stringify(process.env));
+
+var restify = require('restify');
+var mongoose = require('mongoose');
+
+var server = restify.createServer();
+
+mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/test');
+
+var Startup = mongoose.model('Startup', { time: Date });
+Startup.find(function (err, startups) {
+    if (err) console.log('err', err);
+
+    console.log(startups);
+});
+
+var currentStartup = new Startup({ time: new Date() });
+currentStartup.save(function (err) {
+    console.log('Current Startup Time Saved');
+});
+
+server.get('/', restify.serveStatic({
+    directory: './static/html',
+    default: 'index.html'
+}));
+
+
+
+server.listen(process.env.PORT || 8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
